467386adcd6e26a2ff894c152e4df023f93a81c2
Manzuma Halimat Jumai <hallymanz24@gmail.com>
2026-08-12 14:56:36 +0100

    Added a parameter to the logout function.

--- logout.py
+++ logout.py
@@ -1,8 +1,4 @@
-def logout():
-<<<<<<< HEAD
-    print("Thank you for shopping with Women in AI!")
-=======
+def logout(name):
     """logs the user out of the e-commerce website and displays a message/greeting."""
     
-    print("Thank you for shopping with Women in AI!")
->>>>>>> 60cb5e9072b498336a44e3bbbcd992d6df353af7
+    print(f"Thank you for shopping with Women in AI!{name}")
