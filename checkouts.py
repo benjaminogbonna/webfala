cd5b003b95e34b6738fd96bfcf771d8b6a9bad63
Salamat Saheed Togun <saheedsalamat@gmail.com>
2026-08-17 11:09:02 +0100

    added a print message.

--- checkouts.py
+++ checkouts.py
@@ -2,3 +2,4 @@ def checkouts():
     "This function checks out every successful purchase made by a user"
     print(f'Checkouts successful!')
 def confirm_checkouts():
+    print("Checkouts confirmed")
