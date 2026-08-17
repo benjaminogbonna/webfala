fe5e102b4a1fa1d71de5d116cfb812a2991dd5c0
Benjamin <onyedikachi1997@gmail.com>
2026-08-17 11:08:44 +0100

    added a print message.

--- forgot_password.py
+++ forgot_password.py
@@ -3,4 +3,5 @@ def forgot_password(email):
 
 def reset_password(token, new_password):
     print(f"Password has been reset using token: {token}. Your")
-    
+
+print("new password is now set. Please log in with your new password.")
