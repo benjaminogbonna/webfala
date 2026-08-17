ba3343a2c646607072a7e4648b941c1dd69c822b
Manzuma Halimat Jumai <hallymanz24@gmail.com>
2026-08-17 11:09:25 +0100

    Added a print message.

--- logout.py
+++ logout.py
@@ -2,3 +2,5 @@ def logout(name):
     """logs the user out of the e-commerce website and displays a message/greeting."""
     
     print(f"Thank you for shopping with Women in AI!{name}")
+
+    print("This is just a test")
