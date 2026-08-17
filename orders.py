d505b68f2d9b781545846c50b81fa2757057fd64
Ayeni Abiola <ayeniabiola26@gmail.com>
2026-08-17 11:09:40 +0100

    updated amount for oders

--- orders.py
+++ orders.py
@@ -1,3 +1,5 @@
 def orders():
     """it must be delivered before 15th of August 2026"""
-    print(f"300 orders have been made from dangote plc.")
+    print(f"300 orders have been made from dangote plc .")
+def amount(orders):
+    print("the total amount is 31,000,000,000")
