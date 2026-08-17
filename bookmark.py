d67b6dc99779731992f6b6fc0cc491a6790f23f6
oluwafunmilayo <funmiintech@gmail.com>
2026-08-17 11:14:07 +0100

    added bookmark_list

--- bookmark.py
+++ bookmark.py
@@ -2,3 +2,5 @@ def bookmark(save):
     '''this is such a nice website function'''
     print(f'bookmark here')
     print('bookmark is a good innovation')
+def bookmark_list(list):
+    print('bookmark_list')
