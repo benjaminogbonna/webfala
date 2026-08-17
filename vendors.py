7f1a5b331d92258218dfc44866e18163cca922fb
esther <aestheticsolanrewaju@gmail.com>
2026-08-17 11:13:12 +0100

    added another function

--- vendors.py
+++ vendors.py
@@ -2,3 +2,8 @@ def vendors():
     """this describes how to get vendors information"""
     print(f"Vendors information")
     print(f"we have a list of vendors that we work with")
+
+def vendors_contact():
+    """this describes how to get vendors contact information"""
+    print(f"Vendors contact information")
+    print(f"we have a list of vendors contact information that we work with")
