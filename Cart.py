47716ad180312b7c4a5c0b3ba5a5f45539ed7b8f
Jawziyyah <Sogbadez28@gmail.com>
2026-08-17 11:27:30 +0100

    Added a new line

--- Cart.py
+++ Cart.py
@@ -4,9 +4,13 @@ def Cart():
 
 
 def Go_back():
+<<<<<<< HEAD
     print('Would you like to go back to the main page?')
 
 
 print('Hello User')
 
 print('This is the a new line to check commit')
+=======
+    print('Would you like to go back to the main page?')
+>>>>>>> parent of 21ecc90 (Added username to Cart)
