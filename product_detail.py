26d66bb99de4ed56d5f0aafd41dffe9aa8682224
owolabi zainab <owolabiz238@gmail.com>
2026-08-17 11:12:55 +0100

    added new print message

--- product_detail.py
+++ product_detail.py
@@ -1,3 +1,4 @@
 def product_detail():
     """Product_detail contain the information about the product"""
-    print(f"This is the product detail.")
+    print(f"This is the product detail.") 
+    print(product_detail)
