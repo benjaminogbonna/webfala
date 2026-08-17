df742669c86475e5299ff0d5b747e9607f25a40d
Ayeni Abiola <ayeniabiola26@gmail.com>
2026-08-17 11:12:16 +0100

    delivery point added to orders

--- orders.py
+++ orders.py
@@ -3,3 +3,5 @@ def orders():
     print(f"300 orders have been made from dangote plc .")
 def amount(orders):
     print("the total amount is 31,000,000,000")
+def delivery():
+    print("proceed to take your order")
